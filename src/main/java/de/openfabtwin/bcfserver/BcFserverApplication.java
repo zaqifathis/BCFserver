19fc0b00deff7749528cef3c9af236fdb03988b7
Fathis, Zaqi <zaqi.fathis@htw-dresden.de>
2025-11-12 16:05:08 +0100

    init

---
+++ src/main/java/de/openfabtwin/bcfserver/BcFserverApplication.java
@@ -0,0 +1,13 @@
+package de.openfabtwin.bcfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcFserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcFserverApplication.class, args);
+	}
+
+}

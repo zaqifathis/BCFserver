e908725833d3ebf6e95cc5a682d576282f273ef7
Fathis, Zaqi <zaqi.fathis@htw-dresden.de>
2026-01-07 15:45:29 +0100

    update extension

--- src/main/java/de/openfabtwin/entities/ExtensionEntity.java
+++ src/main/java/de/openfabtwin/entities/ExtensionEntity.java
@@ -1,13 +1,10 @@
 package de.openfabtwin.entities;
 
-import de.openfabtwin.generated.dto.ExtensionsGET.*;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.List;
-
 @Entity
-@Table(name = "extensions")
+@Table(name = "project_extensions")
 @Data
 public class ExtensionEntity {
 
@@ -19,57 +16,7 @@ public class ExtensionEntity {
     @JoinColumn(name = "project_id", nullable = false, unique = true)
     private ProjectEntity project;
 
-    @ElementCollection
-    @CollectionTable(name = "ext_topic_type", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> topicType;
-
-    @ElementCollection
-    @CollectionTable(name = "ext_topic_status", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> topicStatus;
-
-    @ElementCollection
-    @CollectionTable(name = "ext_topic_label", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> topicLabel;
-
-    @ElementCollection
-    @CollectionTable(name = "ext_snippet_type", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> snippetType;
-
-    @ElementCollection
-    @CollectionTable(name = "ext_priority", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> priority;
-
-    @ElementCollection
-    @CollectionTable(name = "ext_users", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> users;
-
-    @ElementCollection
-    @CollectionTable(name = "ext_stage", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item")
-    private List<String> stage;
-
-    @ElementCollection(targetClass = ProjectActionsEnum.class)
-    @CollectionTable(name = "ext_project_actions", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private List<ProjectActionsEnum> projectActions;
-
-    @ElementCollection(targetClass = TopicActionsEnum.class)
-    @CollectionTable(name = "ext_topic_actions", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private List<TopicActionsEnum> topicActions;
-
-    @ElementCollection(targetClass = CommentActionsEnum.class)
-    @CollectionTable(name = "ext_comment_actions", joinColumns = @JoinColumn(name = "extension_id"))
-    @Column(name = "item", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private List<CommentActionsEnum> commentActions;
-
+    @Lob
+    @Column(name = "extension_xml", nullable = false)
+    private String extensionXml;
 }
--- src/main/java/de/openfabtwin/repositories/ExtensionRepository.java
+++ src/main/java/de/openfabtwin/repositories/ExtensionRepository.java
@@ -8,6 +8,4 @@ import java.util.Optional;
 public interface ExtensionRepository extends JpaRepository<ExtensionEntity, Long> {
 
     Optional<ExtensionEntity> findByProject_Guid(String guid);
-
-    Optional<ExtensionEntity> findByProjectId(Long projectId);
 }
